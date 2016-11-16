c9a437a4d7936e04cd89e8bc9e9eb1af31430392
Jose M Fernandez-Alba <jm.fernandezalba@commonms.com>
2016-11-16 13:43:31 +0100

    FIX/Disable host check in target proxy server.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ set -ex
 sshpass -p $TARGET_PROXY_PASS \
     ssh -o ProxyCommand="corkscrew $CORPORATE_PROXY_HOST $CORPORATE_PROXY_PORT %h %p" -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 10022:localhost:443 $TARGET_PROXY_USER@$TARGET_PROXY_HOST -p 443 \
         sshpass -p $CONTAINER_PASS \
-            ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -D $TARGET_PROXY_HOST:$TARGET_PROXY_PORT $CONTAINER_USER@localhost -p 10022
+            ssh -nNT -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ServerAliveInterval=60 -D $TARGET_PROXY_HOST:$TARGET_PROXY_PORT $CONTAINER_USER@localhost -p 10022
